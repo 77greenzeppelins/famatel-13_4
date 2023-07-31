ab0e8a6d23802a328fc74edfed27a52aeb4d1e29
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-07-31 08:13:42 +0200

    AnimatePresence_tests_1.6

---
+++ app/nota-prawna/page.tsx
@@ -0,0 +1,11 @@
+/**Components**/
+// import MainWrapper from '@/components/forMultiPage/pageWrappers/MainWrapper/MainWrapper';
+
+export default function NotaPrawnaPage() {
+  /**JSX**/
+  return (
+    // <MainWrapper>
+    <p>NotaPrawnaPage</p>
+    // </MainWrapper>
+  );
+}
