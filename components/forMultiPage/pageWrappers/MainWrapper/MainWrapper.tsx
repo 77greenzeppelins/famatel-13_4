2c9d0f1fb7348f6b5a78e20cb6de937f59b2cbd3
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-07-31 07:55:42 +0200

    AnimatePresence_tests_1.1

--- components/forMultiPage/pageWrappers/MainWrapper/MainWrapper.tsx
+++ components/forMultiPage/pageWrappers/MainWrapper/MainWrapper.tsx
@@ -11,22 +11,13 @@ interface Props {
 }
 
 const variants = {
-  hidden: { opacity: 0, y: -20 },
-  enter: { opacity: 1, y: 0 },
-  exit: { opacity: 0, y: 20 },
+  hidden: { opacity: 0 },
+  enter: { opacity: 1 },
+  exit: { opacity: 0 },
 };
 
 const MainWrapper = ({ children }: Props) => {
   const pathname = usePathname();
-  /*
-  ___1. chould scrollBar position to [0,0] but doesn't...
-  */
-  // const scrollToTop = () => {
-  //   window.scrollTo(0, 0);
-  // };
-  // useEffect(() => {
-  //   scrollToTop();
-  // }, [pathname]);
 
   /**JSX**/
   return (
@@ -42,9 +33,8 @@ const MainWrapper = ({ children }: Props) => {
         animate="enter"
         exit="exit"
         variants={variants}
-        transition={{ type: 'linear', duration: 0, delay: 0 }}
+        transition={{ duration: 0, delay: 0 }}
         // transition={{ type: 'linear', duration: 0.4, delay: 0.2 }}
-
         // className="flex flex-col items-center justify-center min-h-screen "
       >
         <main>{children}</main>
