12c2dc52290ae19761690b21d05493433efe7579
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-07-31 09:22:54 +0200

    AnimatePresence_commonlyKnownIssue_0.0

--- components/forMultiPage/pageWrappers/MainWrapper/MainWrapper.tsx
+++ components/forMultiPage/pageWrappers/MainWrapper/MainWrapper.tsx
@@ -1,5 +1,5 @@
 'use client';
-// import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { usePathname } from 'next/navigation';
 /**FramerMotion Staff*/
 import { motion, AnimatePresence } from 'framer-motion';
@@ -13,31 +13,57 @@ interface Props {
 const variants = {
   hidden: { opacity: 0, y: -20 },
   enter: { opacity: 1, y: 0 },
-  exit: { opacity: 0, y: 20 },
+  // exit: { opacity: 0, y: 20 },
 };
+
+// function handleExitComplete() {
+//   if (typeof window !== 'undefined') {
+//     window.scrollTo({ top: 0 });
+//   }
+// }
 
 const MainWrapper = ({ children }: Props) => {
   const pathname = usePathname();
+
+  /*
+  ___1. 
+  */
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+    timeoutRef.current = setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 0);
+
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, [pathname]);
 
   /**JSX**/
   return (
     // <AnimatePresence
     //   mode="wait"
     //   initial={false}
-    //   onExitComplete={() => window.scrollTo(0, 0)}
+    //   onExitComplete={handleExitComplete}
     // >
     <motion.div
       key={pathname}
       data-component={`MainWrapper-${pathname}`}
       initial="hidden"
       animate="enter"
-      exit="exit"
+      // exit="exit"
       variants={variants}
       // transition={{ duration: 0, delay: 0 }}
       transition={{ type: 'linear', duration: 0.4, delay: 0.2 }}
     >
       <main>{children}</main>
-      {/* <Footer /> */}
+      <Footer />
     </motion.div>
     // </AnimatePresence>
   );
