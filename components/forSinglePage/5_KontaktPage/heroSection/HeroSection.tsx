9e99e8a9c76c8a3062aa85bb4bf92a9d9772af22
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-08-10 13:44:18 +0200

    KontaktPage_3.2

--- components/forSinglePage/5_KontaktPage/heroSection/HeroSection.tsx
+++ components/forSinglePage/5_KontaktPage/heroSection/HeroSection.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 /**Components**/
-import FadingFrame from '@/components/forMultiPage/lines/fadingLine/FadingFrame';
 /**Basic Data*/
 import { styles } from '@/styles';
 
