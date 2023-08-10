3162fee1b6cf2792943371de43fe49b5bca14d35
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-08-10 14:25:36 +0200

    KontaktPage_3.3

--- lib/fmConfigs/framerMotionConfigs.ts
+++ lib/fmConfigs/framerMotionConfigs.ts
@@ -9,3 +9,19 @@ export const imgOverlayVar = {
   initial: { opacity: 1 },
   animate: { opacity: 0, transition: { delay: 0.4, duration: 1 } },
 };
+
+/*
+used in: <KontaktPage> / <HeroSection>
+*/
+export const opacityScaleYDynamicVariants = {
+  from: { opacity: 0, scaleY: 0 },
+  to: (delayVal: number) => ({
+    opacity: 1,
+    scaleY: 1,
+    transition: {
+      duration: 0.8,
+      delay: 0.6 * delayVal,
+      ease: 'easeInOut',
+    },
+  }),
+};
