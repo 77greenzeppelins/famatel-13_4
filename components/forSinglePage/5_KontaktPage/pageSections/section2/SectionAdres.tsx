5b3fd18361f25cdba5ddb5bfba16bdc1e3447e1d
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-08-10 11:21:51 +0200

    KontaktPage_3.0

---
+++ components/forSinglePage/5_KontaktPage/pageSections/section2/SectionAdres.tsx
@@ -0,0 +1,61 @@
+/**Components**/
+import InViewContainer from '@/components/layout/containers/inView/InViewContainer';
+import InViewAnimatedContent from '@/components/layout/containers/inView/InViewAnimatedContent';
+import LargeHeader from '@/components/forMultiPage/headers/largeHeader/LargeHeader';
+import Address from '@/components/layout/rootLayout/footer/bottomSection/contacts/Address';
+import ContactsLinks from '@/components/layout/rootLayout/footer/bottomSection/contacts/ContactsLinks';
+import MapModule from './mapModule/MapModule';
+import WorldMapComponent from '@/components/SVG/maps/WorldMapComponent';
+
+/**Basic Data**/
+import { styles } from '@/styles';
+import { kontaktPageText } from '@/data/textData';
+import { worldMapConfig } from '@/data/basicData';
+
+worldMapConfig;
+
+/**TS**/
+interface Props {
+  containerStyle: string;
+  labelStyle: string;
+  iconStyle: string;
+}
+
+const SectionAdres = ({ containerStyle, labelStyle, iconStyle }: Props) => {
+  /**JSX**/
+  return (
+    <div className="flex items-center">
+      <InViewContainer
+        animationDelay={2}
+        outherContainerStyle="flex flex-col gap-y-20 w-full lg:w-1/2 min-h-[50vh] wrapper-1-l"
+        measuredElementStyle="relative "
+        // topFactor={0.6}
+        topFactor={0.3}
+        bottomFactor={0.3}
+      >
+        <InViewAnimatedContent yFactor="" xFactor="" scaleFactor={0.95}>
+          <div>
+            <div className="flex flex-col w-full gap-y-10">
+              <LargeHeader
+                ornamentStyle={styles.largeHeaderOrnamentStyle}
+                textStyle={`${styles.largeHeaderText} max-w-[10px]`}
+                text={kontaktPageText.headers[1]}
+              />
+              <Address
+                containerStyle={containerStyle}
+                labelStyle={labelStyle}
+                iconStyle={iconStyle}
+                isFooter={false}
+              />
+            </div>
+          </div>
+        </InViewAnimatedContent>
+      </InViewContainer>
+      <div className="hidden w-0 fc lg:w-1/2">
+        <MapModule />
+      </div>
+    </div>
+  );
+};
+
+export default SectionAdres;
