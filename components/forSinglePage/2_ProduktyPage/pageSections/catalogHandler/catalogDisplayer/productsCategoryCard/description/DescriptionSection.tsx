58fc5a2a280b250488bcca71d4e1f5ba9d219e9b
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-08-16 10:37:06 +0200

    ProduktyPage_descriptionSection_1.0

---
+++ components/forSinglePage/2_ProduktyPage/pageSections/catalogHandler/catalogDisplayer/productsCategoryCard/description/DescriptionSection.tsx
@@ -0,0 +1,60 @@
+import Link from 'next/link';
+/**FramerMotion Staff*/
+import ArrowForLinkIcon from '@/components/SVG/icons/heroIcons/ArrowForLinkIcon';
+/**Tailwind Styles**/
+import { styles } from '@/styles';
+/**Framer Motion Staff**/
+import { motion } from 'framer-motion';
+/**Basic Data**/
+import { ProduktyPageText } from '@/data/textData';
+import BasicHeader from '@/components/forMultiPage/headers/basicHeader.tsx/BasicHeader';
+
+const DescriptionSection = ({ i }: { i: number }) => {
+  /** */
+  const { categoriesDescritpions } = ProduktyPageText;
+  /**JSX**/
+  return (
+    <motion.section
+      key="content"
+      initial="collapsed"
+      animate="open"
+      exit="collapsed"
+      variants={{
+        open: { opacity: 1, height: 'auto' },
+        collapsed: { opacity: 0, height: 0 },
+      }}
+      transition={{
+        duration: 0.8,
+        ease: [0.04, 0.62, 0.23, 0.98],
+        //   ease: 'linear',
+        restDelta: 0.0001,
+      }}
+      className="overflow-hidden "
+    >
+      <motion.div
+        variants={{ collapsed: { scale: 0.8 }, open: { scale: 1 } }}
+        transition={{ duration: 0.8 }}
+        className="flex flex-col w-full gap-6 pt-6 origin-top-center"
+      >
+        <Link href="/" className="flex items-center py-1 group w-fit">
+          <span className="w-10 h-full shrink-0 fc">
+            <ArrowForLinkIcon
+              containerStyle={`fc h-6 w-6 min-w-6 min-h-6 aspect-square stroke-corpo group-hover:stroke-light group-hover:rotate-45 origin-center flex-shrink-0 ${styles.lazyAnimation}`}
+            />
+          </span>
+          <p
+            className={`text-grey text-regular group-hover:text-light ${styles.lazyAnimation}`}
+          >
+            Zobacz kategorię
+          </p>
+        </Link>
+        <div className="flex flex-col gap-y-2">
+          <BasicHeader text="opis" />
+          <p className="text-regular text-grey">{categoriesDescritpions[i]}</p>
+        </div>
+      </motion.div>
+    </motion.section>
+  );
+};
+
+export default DescriptionSection;
