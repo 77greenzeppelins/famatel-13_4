3ceaee67170aa15f3c44b561007858816da0fd9d
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-08-17 10:27:07 +0200

    routing_categoriesPagesFolders_1.1

---
+++ data/routingData.ts
@@ -0,0 +1,19 @@
+import { mainPagesPaths } from './basicData';
+/*
+___1.
+*/
+
+export const mainCategoriesPath = [
+  'c1-wtyczki-gniazda',
+  'c2-gniazda-z-blokada',
+  'c3-adaptery-przemyslowe',
+  'c4-gniazda-podwieszane',
+  'c5-przedluzacze-bebnowe',
+  'c6-rozlaczniki-bezpieczenstwa',
+  'c7-ladowarki-samochodowe',
+  'c8-obudowy-i-rozdzielnice',
+  'c9-rozdzielnice-modulowe',
+  'c10-puszki-instalacyjne',
+];
+
+const x = `${mainPagesPaths.produkty}/${mainCategoriesPath[0]}`;
