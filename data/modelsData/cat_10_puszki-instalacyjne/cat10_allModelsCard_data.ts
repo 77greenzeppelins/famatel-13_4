a7eab32e91a837aadac78be22199417f03f1db69
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-08-22 15:44:54 +0200

    _allModelsCard_data.ts_1.1

---
+++ data/modelsData/cat_10_puszki-instalacyjne/cat10_allModelsCard_data.ts
@@ -0,0 +1,23 @@
+import { cat10_subCat1_modelCardsData } from './subCat_1/subCat_1_zamykane_data';
+import { cat10_subCat2_modelCardsData } from './subCat_2/subCat_2_drywall_data';
+import { cat10_subCat3_modelCardsData } from './subCat_3/subCat_3_osadzone_data';
+
+export const cat10_allModelsCard_data: modelCardsDataType[][] = [
+  cat10_subCat1_modelCardsData,
+  cat10_subCat2_modelCardsData,
+  cat10_subCat3_modelCardsData,
+];
+export const cat10_allModels_pathSegments = cat10_allModelsCard_data.map(
+  (subCat, i) => {
+    return subCat.map(({ modelPathSegmant }) => modelPathSegmant);
+  }
+);
+
+// const temp_pathSegments = cat1_allModelsCard_data.map((subCat, i) => {
+//   return subCat.map(({ modelPathSegmant }) => modelPathSegmant);
+// });
+
+// export const cat1_allModels_pathSegments = [
+//   temp_pathSegments.reduce((acc, curr) => [...acc, ...curr], []),
+// ];
+// export const cat1_allModels_pathSegments = [temp_pathSegments.flat()];
