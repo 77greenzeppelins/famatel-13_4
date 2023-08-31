99a37e6fe02f1f0bb5d8fbf09e549eb6ee259891
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-08-31 11:03:00 +0200

    techTablex_variousTypes_1.0

--- components/nestedPagesModels/tableSection/techSpecTable/TechSpecTableHolder.tsx
+++ components/nestedPagesModels/tableSection/techSpecTable/TechSpecTableHolder.tsx
@@ -1,9 +1,11 @@
 /**Components**/
 import TechTableType1 from './techTableType1.tsx/TechTableType1';
+import TechTableType3 from './techTableType3/TechTableType3';
 import Cat8Schema from './cat8Schema/Cat8Schema';
 import OnlyUnderlinedTable from './onlyUnderlinedTable/OnlyUnderlinedTable';
 /**Basic Data**/
 import { catalogStructureData } from '@/data/catalogStructureData';
+import RegularColumnsTable from './regularColumnsTable/RegularColumnsTable';
 
 const TechSpecTableHolder = (props: DynamicModelContent) => {
   /*
@@ -25,8 +27,12 @@ const TechSpecTableHolder = (props: DynamicModelContent) => {
     switch (data.tableType) {
       case 'techTableWtyczkiGniazdaVer1':
         return <TechTableType1 {...data} />;
+      case 'techTableWtyczkiGniazdaVer2':
+        return <TechTableType3 {...data} />;
       case 'onlyUnderlinedTable':
         return <OnlyUnderlinedTable {...data} />;
+      case 'techTableWithRegularColumns':
+        return <RegularColumnsTable {...data} />;
       case 'techSpecTableVer1':
         return <Cat8Schema {...data} />;
     }
