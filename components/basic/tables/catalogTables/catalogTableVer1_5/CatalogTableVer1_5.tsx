47034f658f9752c890338e1120cd203b95a83fda
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-09-03 05:12:28 +0200

    productNavPanel_2.0

--- components/basic/tables/catalogTables/catalogTableVer1_5/CatalogTableVer1_5.tsx
+++ components/basic/tables/catalogTables/catalogTableVer1_5/CatalogTableVer1_5.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
+import { CreateTable1, CreateTable2 } from './handlers/createUI';
+
+/*
+___1. Two "TypeGuards"
+*/
+function isCatalogItemVer1_5aType(
+  item: CatalogItemVer1_5aType | CatalogItemVer1_5bType
+): item is CatalogItemVer1_5aType {
+  return (item as CatalogItemVer1_5aType).line1 !== undefined;
+}
+
+function isCatalogItemVer1_5bType(
+  item: CatalogItemVer1_5aType | CatalogItemVer1_5bType
+): item is CatalogItemVer1_5bType {
+  return (item as CatalogItemVer1_5bType).header !== undefined;
+}
 
 const CatalogTableVer1_5 = (
   data: CatalogItemVer1_5aType | CatalogItemVer1_5bType
 ) => {
-  // const {data.line1,lines} = data
-  console.log('data', data);
+  const createUI = () => {
+    if (isCatalogItemVer1_5aType(data)) {
+      return CreateTable1(data);
+    } else if (isCatalogItemVer1_5bType(data)) {
+      return CreateTable2(data);
+    }
+  };
+
   /**JSX**/
-  return <div>CatalogTableVer1_5</div>;
+  return createUI();
 };
 
 export default CatalogTableVer1_5;
