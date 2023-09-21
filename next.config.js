67a8441fa0eef40256aaf4b60e1eea3572be0d2e
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-09-21 09:20:07 +0200

    next.config.js_2.0

--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        // source: '/kategorie-produktow/:slug*',
-        // destination: '/produkty/:slug*',
-        source: '/kategorie-produktow/*',
-        destination: '/produkty/*',
+        source: '/kategorie-produktow/:slug*',
+        destination: '/produkty/:slug*',
+        // source: '/kategorie-produktow/*',
+        // destination: '/produkty/*',
         permanent: true,
       },
     ];
