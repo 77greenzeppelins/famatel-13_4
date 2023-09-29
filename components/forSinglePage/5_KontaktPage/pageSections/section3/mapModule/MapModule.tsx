ee1cd3f0ff86c4882dbd8f93887069b8e0498de4
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-09-29 16:06:24 +0200

    contactPage_svg-opt_2.0

--- components/forSinglePage/5_KontaktPage/pageSections/section3/mapModule/MapModule.tsx
+++ components/forSinglePage/5_KontaktPage/pageSections/section3/mapModule/MapModule.tsx
@@ -1,43 +1,38 @@
 import Link from 'next/link';
 /**Components**/
-import SvgBielawaMap from '@/components/SVG/maps/SvgBielawaMap_3';
+import SvgBielawaMap from '@/components/SVG/maps/SvgBielawaMap';
 import InViewAnimatedContent from '@/components/layout/containers/inView/InViewAnimatedContent';
 import InViewContainer from '@/components/layout/containers/inView/InViewContainer';
 /**Tailwind Classes**/
 import { styles } from '@/styles';
 
 const MapModule = () => {
-  /**Data Destr...**/
-  const {
-    mapModule: { container, background, foreground },
-  } = styles;
-  /**JSX**/
-  return (
-    <>
-      <Link
-        aria-label={'Link do GoogleMap'}
-        role="link"
-        className="absolute z-10 h-full opacity-50 left-10 right-10"
-        target="_blank"
-        rel="noopener"
-        href="https://www.google.com/maps/search/Famatel+Polska+Sp.o.o+ul.+Willowa+5,+58+260+Bielawa+Polska"
-        scroll={false}
-      />
-      <InViewContainer
-        outherContainerStyle="z-5 w-full h-full"
-        animationDelay={2}
-        topFactor={0.3}
-        bottomFactor={0.1}
-      >
-        <InViewAnimatedContent scaleFactor={0.95} xFactor={''} yFactor={''}>
-          <div className={container}>
-            <div className={background} />
-            <SvgBielawaMap containerStyle={foreground} />
-          </div>
-        </InViewAnimatedContent>
-      </InViewContainer>
-    </>
-  );
+    /**Data Destr...**/
+    const {
+        mapModule: { container, background, foreground }
+    } = styles;
+    /**JSX**/
+    return (
+        <>
+            <Link
+                aria-label={'Link do GoogleMap'}
+                role="link"
+                className="absolute z-10 h-full opacity-50 left-10 right-10"
+                target="_blank"
+                rel="noopener"
+                href="https://www.google.com/maps/search/Famatel+Polska+Sp.o.o+ul.+Willowa+5,+58+260+Bielawa+Polska"
+                scroll={false}
+            />
+            <InViewContainer outherContainerStyle="z-5 w-full h-full" animationDelay={2} topFactor={0.3} bottomFactor={0.1}>
+                <InViewAnimatedContent scaleFactor={0.95} xFactor={''} yFactor={''}>
+                    <div className={container}>
+                        <div className={background} />
+                        <SvgBielawaMap containerStyle={foreground} />
+                    </div>
+                </InViewAnimatedContent>
+            </InViewContainer>
+        </>
+    );
 };
 
 export default MapModule;
--- public/images/1_homePage/index.ts
+++ public/images/1_homePage/index.ts
@@ -1,8 +1,8 @@
 // no need to make any import: import dirtyPlug from './dirtyPlug_1200_1200.webp';
 export const dirtyPlugImage: ImageType_C = {
-  path: '/images/1_homePage/dirtyPlug_1200_1200.png',
-  // path:"/public/images/1_homePage/dirtyPlug_1200_1200.png" // (!) is wrong
-  width: 1200,
-  height: 1200,
-  alt: 'Wtyczka przemysłowa używana w trudnych warunkach',
+    path: '/images/1_homePage/dirtyPlug_1200_1200.webp',
+    // path:"/public/images/1_homePage/dirtyPlug_1200_1200.png" // (!) is wrong
+    width: 1200,
+    height: 1200,
+    alt: 'Wtyczka przemysłowa używana w trudnych warunkach'
 };
