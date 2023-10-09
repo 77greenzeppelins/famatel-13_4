ab6ebec244b7263d76935b842c14a3d48dfe8024
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-10-09 13:12:06 +0200

    homePage_Image_1.1

--- components/forSinglePage/1_HomePage/heroSection/graphicSection/GraphicSection.tsx
+++ components/forSinglePage/1_HomePage/heroSection/graphicSection/GraphicSection.tsx
@@ -5,27 +5,28 @@ import Sniper from '@/components/forMultiPage/accents/sniper/Sniper';
 import PseudoTechPanel from './pseudoTechPanel/PseudoTechPanel';
 
 const GraphicSection = () => {
-  /**JSX**/
-  return (
-    <div className="relative flex justify-end w-full h-full">
-      <Image
-        className="aspect-square"
-        alt={dirtyPlugImage.alt}
-        // src={dirtyPlugImg}
-        src={dirtyPlugImage.path}
-        width={dirtyPlugImage.width}
-        height={dirtyPlugImage.height}
-        // fill
-        // sizes="1200"
-        // fill // intrinsic|fixed|responsive|fill allowed;  fill your parent
-      />
-      <div className="absolute hidden xs3xx:flex bottom-[10%] right-[25%] h-[70%] w-[70%]">
-        <Sniper isHovered={true} />
-        <PseudoTechPanel />
-      </div>
-      <ScrollPrompt />
-    </div>
-  );
+    /**JSX**/
+    return (
+        <div className="relative flex justify-end w-full h-full overflow-hidden">
+            <Image
+                className="aspect-square"
+                alt={dirtyPlugImage.alt}
+                src={dirtyPlugImage.path}
+                width={dirtyPlugImage.width}
+                height={dirtyPlugImage.height}
+                priority
+                // fill={true}
+                // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                // className="object-cover w-full h-full aspect-square"
+                // fill // intrinsic|fixed|responsive|fill allowed;  fill your parent
+            />
+            <div className="absolute hidden xs3xx:flex bottom-[10%] right-[25%] h-[70%] w-[70%]">
+                <Sniper isHovered={true} />
+                <PseudoTechPanel />
+            </div>
+            <ScrollPrompt />
+        </div>
+    );
 };
 
 export default GraphicSection;
